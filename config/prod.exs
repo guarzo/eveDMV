f8e6d10731264853f68ef413813364516b606513
guarzo <guarzo.eve@gmail.com>
2025-07-14 15:46:06 +0000

    prod

--- config/prod.exs
+++ config/prod.exs
@@ -9,8 +9,7 @@ config :eve_dmv, environment: :prod
 # which you should run after static files are built and
 # before starting your production server.
 config :eve_dmv, EveDmvWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: EveDmv.Finch
